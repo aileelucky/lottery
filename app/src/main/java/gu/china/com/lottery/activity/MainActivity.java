321794695ef79ea952d6178f5d3d8b8e1768b682
jiajia.gu <jiajia.gu@uama.com.cn>
2017-08-03 13:53:18 +0800

    first submit

---
+++ app/src/main/java/gu/china/com/lottery/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package gu.china.com.lottery.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gu.china.com.lottery.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
